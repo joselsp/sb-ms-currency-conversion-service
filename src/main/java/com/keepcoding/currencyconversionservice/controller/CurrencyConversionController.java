78ccbdb6ccf856a863d2a30b1e875b25434a63e8
Jose Luis Solis <solis.jl@gmail.com>
2020-07-21 08:07:18 -0700

    Added Feign Rest Client

--- src/main/java/com/keepcoding/currencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/com/keepcoding/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,29 +1,25 @@
 package com.keepcoding.currencyconversionservice.controller;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
+import com.keepcoding.currencyconversionservice.client.CurrencyExchangeServiceProxy;
 import com.keepcoding.currencyconversionservice.model.CurrencyConversionBean;
 
 @RestController
 public class CurrencyConversionController {
 	
+	@Autowired
+	CurrencyExchangeServiceProxy currencyExchangeServiceProxy;
+	
 	@GetMapping("/currency-converter/from/{from}/to/{to}/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable("from") String from, @PathVariable("to") String to, @PathVariable("quantity") BigDecimal quantity) {
 		
-		Map<String, String> uriVars = new HashMap<>();
-		uriVars.put("from", from);
-		uriVars.put("to", to);
-		
-		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVars);
-		CurrencyConversionBean result = responseEntity.getBody();
+		CurrencyConversionBean result = currencyExchangeServiceProxy.retrieveExchangeValue(from, to);
 		
 		return new CurrencyConversionBean(result.getId(), result.getFrom(), result.getTo(), result.getConversionMultiple(), quantity, quantity.multiply(result.getConversionMultiple()), 8100);
 	}
