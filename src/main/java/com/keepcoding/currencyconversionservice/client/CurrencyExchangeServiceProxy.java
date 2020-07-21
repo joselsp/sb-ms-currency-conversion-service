8909a74a6764a190244b6d2475362fe6f37b3472
Jose Luis Solis <solis.jl@gmail.com>
2020-07-21 12:49:37 -0700

    Implemented Zuul Api Gateway

--- src/main/java/com/keepcoding/currencyconversionservice/client/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/keepcoding/currencyconversionservice/client/CurrencyExchangeServiceProxy.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.keepcoding.currencyconversionservice.model.CurrencyConversionBean;
 
-@FeignClient(name = "currency-exchange-service")
-@RibbonClient(name = "currency-exchange-service")
+@FeignClient(name = "zuul-api-gateway-service")
+@RibbonClient(name = "zuul-api-gateway-service")
 public interface CurrencyExchangeServiceProxy {
 	
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	CurrencyConversionBean retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
 }
